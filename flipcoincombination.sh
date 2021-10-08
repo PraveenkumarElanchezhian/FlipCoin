f2c1a06ffa8cd6831b0da32082dabb9aebfaf312
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-08 09:53:24 +0530

    shellprogram

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
+headcount=0
+tailcount=0
+
+for ((i=1; i<=5; i++))
+do
+
 random=$(($RANDOM%2))
 if [ $random -eq 1 ]
 then
-echo "Head";
+headcount=$(($headcount+1))
 else
-echo "Tail";
+tailcount=$(($tailcount+1))
 fi
+diff_one=$(($headcount-$tailcount))
+diff_two=$(($tailcount-$headcount))
+done
+
+echo "Head on : $headcount times"
+echo "Tail on : $tailcount times"
+
+head_percentage=$(( ($headcount/5) * 100))
+tail_percentage=$((100 - $head_percentage))
+
+declare -A percentage;
+percentage[head]=$head_percentage;
+percentage[tail]=$tail_percentage;
+
+echo "percentage of dictionary : ${percentage[head]}";
+echo "percentage of dictionary : ${percentage[tail]}";
+
+
